e3a40a99015b5550c826228e47373d64a2a8a8b5
Filip Axani <filip.axani@gmail.com>
2017-10-03 10:18:39 +0200

    Show token text in admin overview

--- CustomLogger/tokenizer/models.py
+++ CustomLogger/tokenizer/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Token(models.Model):
     token = models.CharField(max_length=24)
+
+    def __str__(self):
+            return self.token
