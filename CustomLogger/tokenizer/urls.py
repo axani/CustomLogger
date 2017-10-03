ba687e498827dc45955b3bbc9dbf04ff5dfa6e9a
Filip Axani <filip.axani@gmail.com>
2017-10-03 14:11:09 +0200

    Add view to create token

--- CustomLogger/tokenizer/urls.py
+++ CustomLogger/tokenizer/urls.py
@@ -3,8 +3,10 @@ from django.conf import settings
 
 from . import views
 
+app_name = 'tokenizer'
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^create/$', views.create_token, name='create'),
     url(r'^%s/(?P<entered_token>.*)' % settings.TOKEN_SUBDOMAIN, views.token_page, name='token_page'),
- #   url(r'^$', views.index, name='index'),
 
 ]
--- CustomLogger/tokenizer/views.py
+++ CustomLogger/tokenizer/views.py
@@ -8,5 +8,7 @@ def index(request):
 
 def token_page(request, entered_token):
     token = get_object_or_404(Token, token=entered_token)
-
     return render(request, 'tokenizer/token_page.html', {'token': entered_token})
+
+def create_token(request):
+    return render(request, 'tokenizer/token_page.html', {'token': 'success'})
