23465984444f87016483989aa09f2e2090042308
Filip Axani <filip.axani@gmail.com>
2017-10-03 14:31:52 +0200

    randomly create a token

--- CustomLogger/tokenizer/views.py
+++ CustomLogger/tokenizer/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, Http404
 from .models import Token
+from random import choice
+from string import ascii_lowercase
 
 # Create your views here.
 def index(request):
@@ -11,4 +13,8 @@ def token_page(request, entered_token):
     return render(request, 'tokenizer/token_page.html', {'token': entered_token})
 
 def create_token(request):
-    return render(request, 'tokenizer/token_page.html', {'token': 'success'})
+    length = Token._meta.get_field('token').max_length
+    letters = ascii_lowercase
+    generated_token = ''.join([choice(letters) for _ in range(length)])
+    new_token = generated_token
+    return render(request, 'tokenizer/token_page.html', {'token': new_token})
